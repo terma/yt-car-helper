7a18c9a1834b056013516d8b85ce785eaf5f7106
terma <artem.stasuk@gmail.com>
2023-09-21 23:07:20 -0700

    Add phone activity to show help message.

---
+++ src/main/java/com/terma/ytcarhelper/MainActivity.java
@@ -0,0 +1,28 @@
+package com.terma.ytcarhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+
+import com.terma.ytcarhelper.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+}
